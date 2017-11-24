fbfd9d4898113a89d3fd148527a19290a0e0eebe
Janne Kemppainen <ext-janne.k.kemppainen@elisa.fi>
2017-11-24 22:49:01 +0200

    Show maps

---
+++ complex/main.py
@@ -0,0 +1,36 @@
+from appJar import gui
+from random import randint
+
+import tkinter as tk
+
+from complex.application import Application
+import os
+from Cocoa import NSRunningApplication, NSApplicationActivateIgnoringOtherApps
+
+WINDOW_WIDTH = 1400
+WINDOW_HEIGHT = 900
+
+
+def main():
+    root = tk.Tk()
+    root.resizable(width=False, height=False)
+    root.geometry('{}x{}'.format(WINDOW_WIDTH, WINDOW_HEIGHT))
+    app = Application(master=root)
+
+    app.load_map("resources/minimaps/albasrah_minimap.gif")
+
+    # Move to foreground
+    running_app = NSRunningApplication.runningApplicationWithProcessIdentifier_(os.getpid())
+    running_app.activateWithOptions_(NSApplicationActivateIgnoringOtherApps)
+
+    app.mainloop()
+
+
+
+
+def get_player_positions():
+    return [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for a in range(50)]
+
+
+if __name__ == "__main__":
+    main()
---
+++ complex/resources/minimaps/convert.sh
@@ -0,0 +1,1 @@
+for i in *.jpg; do sips -s format gif $i --out $i.gif; done
