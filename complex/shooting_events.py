15418b9d4b8f894c4e3b4f50cdf2b27ca84041b7
Janne Kemppainen <ext-janne.k.kemppainen@elisa.fi>
2017-11-26 10:00:27 +0200

    Fix shooting events

--- complex/shooting_events.py
+++ complex/shooting_events.py
@@ -54,10 +54,11 @@ class ShootingEvents:
         events = []
         for key in self.shooting_data:
             if key in range(timestamp, timestamp + SHOOTING_EVENT_WINDOW):
-                attacker_pos = self.scale_position(self.shooting_data[key][0][0])
-                victim_pos = self.scale_position(self.shooting_data[key][0][1])
+                for shooting in self.shooting_data[key]:
+                    attacker_pos = self.scale_position(shooting[0])
+                    victim_pos = self.scale_position(shooting[1])
 
-                events.append(ShootingEvent(self.screen, attacker_pos, victim_pos))
+                    events.append(ShootingEvent(self.screen, attacker_pos, victim_pos))
         return events
 
 
